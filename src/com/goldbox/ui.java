3978109cac5d41ca2a3e9a593aac961fa4ded080
George Dorn <gdorn@crunchbang.(none)>
2011-01-03 08:26:14 -0800

    Adding AndEngine lib and UI stubs

---
+++ src/com/goldbox/ui.java
@@ -0,0 +1,33 @@
+package com.goldbox;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+
+public class ui extends BaseGameActivity {
+
+	@Override
+	public void onLoadComplete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Engine onLoadEngine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onLoadResources() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
